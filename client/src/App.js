4c9a2cde1de706122cfd3bcb5a50d0b323c1dd13
Xiaoping Li <narci2008@gmail.com>
2018-04-28 08:25:43 -0700

    console.log test

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,9 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Home extends Component {
+  
   render() {
+    console.log('welcome home');
     return (
       <div className="App">
         <h1>Welcome to Patients Files Manager System</h1>

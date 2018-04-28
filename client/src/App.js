0da234501bcee72c2470aaedd63d20105bd15962
Xiaoping Li <narci2008@gmail.com>
2018-04-28 08:50:51 -0700

    Remove console.log test from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import './App.css';
 class Home extends Component {
   
   render() {
-    console.log('welcome home');
     return (
       <div className="App">
         <h1>Welcome to Patients Files Manager System</h1>
